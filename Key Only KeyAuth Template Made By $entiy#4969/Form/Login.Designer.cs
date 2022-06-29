25d47d5d757b6196736e7a587f4a32260f5521cc
SentiyFn <108193052+SentiyFn@users.noreply.github.com>
2022-06-28 21:51:05 -0500

    Add files via upload

---
+++ Key Only KeyAuth Template Made By $entiy#4969/Form/Login.Designer.cs
@@ -0,0 +1,190 @@
+namespace KeyAuth
+{
+    // Token: 0x02000002 RID: 2
+    public partial class Login : global::System.Windows.Forms.Form
+    {
+        // Token: 0x06000011 RID: 17 RVA: 0x0000215C File Offset: 0x0000035C
+        protected override void Dispose(bool disposing)
+        {
+            bool flag = disposing && this.components != null;
+            if (flag)
+            {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // Token: 0x06000012 RID: 18 RVA: 0x00002194 File Offset: 0x00000394
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Siticone.UI.AnimatorNS.Animation animation3 = new Siticone.UI.AnimatorNS.Animation();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.siticoneDragControl1 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.siticoneControlBox1 = new Siticone.UI.WinForms.SiticoneControlBox();
+            this.siticoneTransition1 = new Siticone.UI.WinForms.SiticoneTransition();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginBtn = new Siticone.UI.WinForms.SiticoneRoundedButton();
+            this.key = new Siticone.UI.WinForms.SiticoneRoundedTextBox();
+            this.siticoneShadowForm = new Siticone.UI.WinForms.SiticoneShadowForm(this.components);
+            this.SuspendLayout();
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this;
+            // 
+            // siticoneControlBox1
+            // 
+            this.siticoneControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox1.BorderRadius = 10;
+            this.siticoneTransition1.SetDecoration(this.siticoneControlBox1, Siticone.UI.AnimatorNS.DecorationType.None);
+            this.siticoneControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.siticoneControlBox1.HoveredState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.siticoneControlBox1.HoveredState.IconColor = System.Drawing.Color.White;
+            this.siticoneControlBox1.HoveredState.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.IconColor = System.Drawing.Color.White;
+            this.siticoneControlBox1.Location = new System.Drawing.Point(283, 4);
+            this.siticoneControlBox1.Name = "siticoneControlBox1";
+            this.siticoneControlBox1.ShadowDecoration.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.siticoneControlBox1.TabIndex = 1;
+            this.siticoneControlBox1.Click += new System.EventHandler(this.siticoneControlBox1_Click);
+            // 
+            // siticoneTransition1
+            // 
+            this.siticoneTransition1.AnimationType = Siticone.UI.AnimatorNS.AnimationType.Rotate;
+            this.siticoneTransition1.Cursor = null;
+            animation3.AnimateOnlyDifferences = true;
+            animation3.BlindCoeff = ((System.Drawing.PointF)(resources.GetObject("animation3.BlindCoeff")));
+            animation3.LeafCoeff = 0F;
+            animation3.MaxTime = 1F;
+            animation3.MinTime = 0F;
+            animation3.MosaicCoeff = ((System.Drawing.PointF)(resources.GetObject("animation3.MosaicCoeff")));
+            animation3.MosaicShift = ((System.Drawing.PointF)(resources.GetObject("animation3.MosaicShift")));
+            animation3.MosaicSize = 0;
+            animation3.Padding = new System.Windows.Forms.Padding(50);
+            animation3.RotateCoeff = 1F;
+            animation3.RotateLimit = 0F;
+            animation3.ScaleCoeff = ((System.Drawing.PointF)(resources.GetObject("animation3.ScaleCoeff")));
+            animation3.SlideCoeff = ((System.Drawing.PointF)(resources.GetObject("animation3.SlideCoeff")));
+            animation3.TimeCoeff = 0F;
+            animation3.TransparencyCoeff = 1F;
+            this.siticoneTransition1.DefaultAnimation = animation3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.siticoneTransition1.SetDecoration(this.label1, Siticone.UI.AnimatorNS.DecorationType.None);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 10F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 19);
+            this.label1.TabIndex = 22;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.siticoneTransition1.SetDecoration(this.label2, Siticone.UI.AnimatorNS.DecorationType.None);
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(2, 4);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 19);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Loader Login";
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.BorderColor = System.Drawing.Color.Orange;
+            this.LoginBtn.BorderThickness = 1;
+            this.LoginBtn.CheckedState.Parent = this.LoginBtn;
+            this.LoginBtn.CustomImages.Parent = this.LoginBtn;
+            this.siticoneTransition1.SetDecoration(this.LoginBtn, Siticone.UI.AnimatorNS.DecorationType.None);
+            this.LoginBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.LoginBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginBtn.ForeColor = System.Drawing.Color.White;
+            this.LoginBtn.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
+            this.LoginBtn.HoveredState.Parent = this.LoginBtn;
+            this.LoginBtn.Location = new System.Drawing.Point(91, 77);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.ShadowDecoration.Parent = this.LoginBtn;
+            this.LoginBtn.Size = new System.Drawing.Size(151, 27);
+            this.LoginBtn.TabIndex = 26;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // key
+            // 
+            this.key.AllowDrop = true;
+            this.key.BorderColor = System.Drawing.Color.Orange;
+            this.key.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneTransition1.SetDecoration(this.key, Siticone.UI.AnimatorNS.DecorationType.None);
+            this.key.DefaultText = "Key";
+            this.key.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.key.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.key.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.key.DisabledState.Parent = this.key;
+            this.key.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.key.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.key.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.key.FocusedState.Parent = this.key;
+            this.key.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.key.HoveredState.Parent = this.key;
+            this.key.Location = new System.Drawing.Point(46, 40);
+            this.key.Margin = new System.Windows.Forms.Padding(4);
+            this.key.Name = "key";
+            this.key.PasswordChar = '\0';
+            this.key.PlaceholderText = "";
+            this.key.SelectedText = "";
+            this.key.ShadowDecoration.Parent = this.key;
+            this.key.Size = new System.Drawing.Size(236, 30);
+            this.key.TabIndex = 32;
+            this.key.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.ClientSize = new System.Drawing.Size(332, 113);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.siticoneControlBox1);
+            this.siticoneTransition1.SetDecoration(this, Siticone.UI.AnimatorNS.DecorationType.BottomMirror);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loader";
+            this.TransparencyKey = System.Drawing.Color.Maroon;
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        // Token: 0x04000001 RID: 1
+        private global::System.ComponentModel.IContainer components = null;
+
+        // Token: 0x04000002 RID: 2
+        private global::Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl1;
+
+        // Token: 0x04000004 RID: 4
+        private global::Siticone.UI.WinForms.SiticoneControlBox siticoneControlBox1;
+
+        // Token: 0x04000009 RID: 9
+        private global::Siticone.UI.WinForms.SiticoneTransition siticoneTransition1;
+
+        // Token: 0x0400000A RID: 10
+        private global::System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Siticone.UI.WinForms.SiticoneRoundedButton LoginBtn;
+        private Siticone.UI.WinForms.SiticoneShadowForm siticoneShadowForm;
+        private Siticone.UI.WinForms.SiticoneRoundedTextBox key;
+    }
+}
---
+++ Key Only KeyAuth Template Made By $entiy#4969/Form/Main.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyAuth
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void siticoneControlBox1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
